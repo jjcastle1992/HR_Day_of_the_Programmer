d0a48129fee9a96fb2602f5b9ad092e4f18cd3f5
James <jjcastle@utexas.edu>
2022-06-14 19:30:02 -0500

    Basic plan for building dayOfProgrammer mapped out.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,27 @@ string rtrim(const string &);
  */
 
 string dayOfProgrammer(int year) {
+    string dateOfProgrammer = "";
+    bool leapYear = false;
+    int daysOfTheMonth [] {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+    //Intake Year
+
+    //Determine if the date is 1917 or before or 1918 or after
+
+    //determine if it is a leap year
+
+    // If it is 1917 or prior, leap years are divisible only by 4
+
+    //if it is 1918 or after, leap years are divisible by 400 OR divisible by 4 but not 100.
+
+    //update an array of days in the month, of size 12, w/ elements 1-12 corresponding to Jan-Dec.
+    // Note: Element 0 is not used to align index w/ month. All months except Feb will be constant.
+    // Feb will equal 28 unless in a leap year. If leap year, then Feb = 29
+
+    //Find the 256th day of the year output as dd.mm.yyyy
+
+    return dateOfProgrammer;
 }
 
 int main()
